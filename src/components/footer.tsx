0f7925bdecbd22f3dc13617b0b155076ed23a4ab
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-04-06 00:03:37 -0400

    Change copy, add header and footer components

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ROOT_CLASS = "footer-container";
+
+function Footer() {
+  return (
+    <footer className={ROOT_CLASS}>
+      <p className="copy-right">©2023 Made with 🌵 in Arizona</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ROOT_CLASS = "header-container";
+
+function Header() {
+  return (
+    <header className={ROOT_CLASS}>
+      <h1 className="logo">CactAI 🌵</h1>
+    </header>
+  );
+}
+
+export default Header;
